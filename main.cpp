b99508fabb9e8825a865c03169de37837301f663
Vale088 <valerio.sinni@marconibari.edu.it>
2024-10-24 08:09:15 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n,i; 
+    float numero,max;
 
+    cout<<"inserisci il numero"<<endl;
+    cin>>n;
+    
+    i=1;
+    while (i<=n) { 
+        i=i+1;
+        if (i=1){ 
+        max=numero;
+        } else { 
+            if(numero>max){
+            max=numero; 
+            }
+        }   
+    }
+    cout<<"il massimo numero e " << max <<endl; 
+}
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
